74631b2257fa74669c0ab9a216b5fcbd22c3e184
vincenzo <vincenzofarro01@gmail.com>
2025-06-18 03:38:13 +0200

    c11 - refactor: standardize CSS structure and update content

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -32,16 +32,16 @@ const projectsData = [
         title: "Mobile Legends Draft Tool",
         description: `Tool sviluppato in Python che analizza le scelte del team e degli avversari nella fase di draft del gioco Mobile Legends. Utilizza Pandas per filtrare e valutare combinazioni ottimali, suggerendo gli eroi più adatti basandosi su sinergie di squadra e counter contro i nemici. È uno script pensato per giocatori competitivi o per chi vuole un vantaggio tattico. Il progetto è documentato ed espandibile.`,
         technologies: ["Python", "Pandas"],
-        gitLink: "https://github.com/tuo-username/mobile-legends-draft-tool",
-        webLink: "https://mlbbdrafttool.com",
+        gitLink: "https://github.com/vin-03/mobile-legends-draft-tool",
+        webLink: "https://github.com/vin-03/mobile-legends-draft-tool",
         img: "/imgs/mlbbdrafttool.png"
     },
     {
         title: "Portfolio Web Personale",
         description: `Questo portfolio è stato sviluppato con Next.js sfruttando l'App Router. Include sezioni come Resume, Projects e Contact. Lo stile è personalizzato con CSS Modules e design minimalista. Il codice è organizzato per facilitare l’aggiunta di nuovi progetti e contenuti. È pensato per essere sia vetrina che laboratorio personale.`,
         technologies: ["Next.js", "React", "CSS Modules"],
-        gitLink: "https://github.com/tuo-username/portfolio",
-        webLink: "https://tuoportfolio.it",
+        gitLink: "https://github.com/vin-03/portfolio-vincenzofarro",
+        webLink: "https://vincenzofarro-portfolio.netlify.app/ ",
         img: "/imgs/portfolioscreenshot.png"
     }
 ]
